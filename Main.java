3d4c76c045a56974857049dde2df5f9828e28e73
Maria Pore <mariapore06@gmail.com>
2025-09-29 19:17:46 +0100

    outline

---
+++ Main.java
@@ -0,0 +1,20 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        // java banking program
+
+        // display menu
+
+        // get and process user choice
+
+        // showBalance()
+
+        // deposit()
+
+        //withdraw()
+
+        // EXIT MESSAGE
+    }
+    
+}
