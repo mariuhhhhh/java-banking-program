9c5f436791eeed3cf57f0d72ce17929fcdb32d1e
Maria Pore <mariapore06@gmail.com>
2025-09-29 19:25:21 +0100

    display menu

--- Main.java
+++ Main.java
@@ -1,10 +1,25 @@
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
 
         // java banking program
 
+        Scanner scanner = new Scanner(System.in);
+        double balance;
+        boolean isRunning = true;
+        int choice;
+
         // display menu
+        System.out.println("***************"); //seperaters
+        System.out.println("BANKING PROGRAM");
+        System.out.println("***************"); //seperaters
+        System.out.println("1. Show Balance");
+        System.out.println("2. Deposit");
+        System.out.println("3. Withdraw");
+        System.out.println("4. Exit");
+        System.out.println("***************"); //seperaters
 
         // get and process user choice
 
@@ -15,6 +30,8 @@ public class Main {
         //withdraw()
 
         // EXIT MESSAGE
+
+        scanner.close();
     }
     
 }
