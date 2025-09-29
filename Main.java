0af48779890588672763d6100f4ee9f00b6d81e6
Maria Pore <mariapore06@gmail.com>
2025-09-29 23:46:24 +0100

    while loop

--- Main.java
+++ Main.java
@@ -11,28 +11,32 @@ public class Main {
         boolean isRunning = true;
         int choice;
 
-        // display menu
-        System.out.println("***************"); //seperaters
-        System.out.println("BANKING PROGRAM");
-        System.out.println("***************"); //seperaters
-        System.out.println("1. Show Balance");
-        System.out.println("2. Deposit");
-        System.out.println("3. Withdraw");
-        System.out.println("4. Exit");
-        System.out.println("***************"); //seperaters
+        while(isRunning) {
+            // display menu
+            System.out.println("***************"); //seperaters
+            System.out.println("BANKING PROGRAM");
+            System.out.println("***************"); //seperaters
+            System.out.println("1. Show Balance");
+            System.out.println("2. Deposit");
+            System.out.println("3. Withdraw");
+            System.out.println("4. Exit");
+            System.out.println("***************"); //seperaters
 
-        // get and process user choice
+            // get and process user choice
 
-        System.out.print("Enter your choice (1-4): ");
-        choice = scanner.nextInt();
+            System.out.print("Enter your choice (1-4): ");
+            choice = scanner.nextInt();
 
-        switch(choice){ 
-            case 1 -> System.out.println("SHOW BALANCE");
-            case 2 -> System.out.println("DEPOSIT");
-            case 3 -> System.out.println("WITHDRAW");
-            case 4 -> isRunning = false;
-            default -> System.out.println("INVALID CHOICE");
+            switch(choice){ 
+                case 1 -> System.out.println("SHOW BALANCE");
+                case 2 -> System.out.println("DEPOSIT");
+                case 3 -> System.out.println("WITHDRAW");
+                case 4 -> isRunning = false;
+                default -> System.out.println("INVALID CHOICE");
+            }
+
         }
+
 
         // showBalance()
 
